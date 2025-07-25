192416a408a8bb3dfe7a1f6118ee67a1f199cfc2
Aditya Anand <aditya.anand0112358@gmail.com>
2025-07-25 05:39:18 +0530

    Update devtimer.rb

--- devtimer.rb
+++ devtimer.rb
@@ -2,7 +2,7 @@ class devtimer < Formula
  desc "Simple dev timer for productivity"
  homepage "https://github.com/adi0112358/devtimer"
  url "https://github.com/adi0112358/devtimer/archive/refs/tags/v1.0.0.tar.gz"
- sha256 "REPLACE_ME_WITH_REAL_SHA256"
+ sha256 "da263394b0148ab61062f7e9ee549a126e3bb7e1d2ff560c70be03a3ad92f2f5"
  license "MIT"
  
   def install
